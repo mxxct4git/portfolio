7a4a4a0682030fc4dd38fa69454de7ef23d59d5a
mxxct4git <386965035@qq.com>
2025-01-15 13:29:10 +0800

    修改page参数

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,8 +1,8 @@
-type pageParam = {
+type ProjectParams = {
   id: string;
 };
 
-export default async function Project({ params }: { params: pageParam }) {
-  const resolveParam = await Promise.resolve(params);
-  return <h1> Project: {resolveParam.id} </h1>;
+export default async function Project({ params }: { params: ProjectParams }) {
+  const { id } = await params;
+  return <h1> Project: {id} </h1>;
 }
